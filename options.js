e0d2a3b9df434da91a9697d362ef69a22b0b0c23
Anton Khorev <tony29@yandex.ru>
2021-10-01 14:32:34 +0300

    will read options from text file

--- options.js
+++
@@ -1,5 +0,0 @@
-console.log('options RUN!!!')
-const $input=document.getElementById('settings-otrs')
-$input.addEventListener('change',(ev)=>{
-	console.log('option changed',$input.value)
-})
--- panel.js
+++ panel.js
@@ -1,1 +1,25 @@
 console.log('panel RUN!!!')
+document.getElementById('action-create-ticket').addEventListener('click',()=>{
+	alert('create ticket CLICK!!')
+})
+document.getElementById('settings-load').addEventListener('change',readSettings)
+document.getElementById('settings-sample').addEventListener('click',downloadSampleSettings)
+
+function readSettings() {
+	const [file]=this.files
+	console.log('picked file',file)
+	const reader=new FileReader()
+	reader.addEventListener('load',()=>{
+		console.log('read this:',reader.result)
+	})
+	reader.readAsText(file)
+}
+
+function downloadSampleSettings() {
+	const blob=new Blob([sampleSettingsFile],{type:'text/plain'})
+	const url=URL.createObjectURL(blob)
+	browser.downloads.download({url,filename:'osm-dwg-helper-settings.txt',saveAs:true})
+}
+
+const sampleSettingsFile=`otrs = https://otrs.example.com/
+`
