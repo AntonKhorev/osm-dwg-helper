486e630d9db683750ed2fd5cc71c0486004c6baf
Anton Khorev <tony29@yandex.ru>
2021-10-22 18:33:23 +0300

    inform about new ticket on otrs login page

--- content-create-ticket.js
+++ content-create-ticket.js
@@ -6,29 +6,50 @@ if (!window.osmDwgHelperCreateTicketListenerInstalled) {
 function messageListener(message) {
 	if (message.action!='addIssueDataToTicket') return false
 	const $form=document.getElementById('NewPhoneTicket')
-	if (!$form) return Promise.reject("can't find form") // TODO detect login page
-	feedValues($form.FromCustomer,message.ticketData.FromCustomers)
+	if ($form) {
+		populateTicketForm($form,message.ticketData)
+		return Promise.resolve()
+	}
+	const $loginBox=document.getElementById('LoginBox')
+	if ($loginBox) {
+		informOnLoginScreen($loginBox)
+		return Promise.reject("on login page")
+	}
+	return Promise.reject("met unknown webpage content")
+}
+
+function populateTicketForm($form,ticketData) {
+	feedValues($form.FromCustomer,ticketData.FromCustomers)
 	selectFirstOption($form.Dest)
-	if (message.ticketData.Subject!=null) {
-		$form.Subject.value=message.ticketData.Subject
+	if (ticketData.Subject!=null) {
+		$form.Subject.value=ticketData.Subject
 		$form.Subject.dispatchEvent(new Event('change'))
 	}
-	if (message.ticketData.Body!=null) {
-		$form.Body.value=message.ticketData.Body
+	if (ticketData.Body!=null) {
+		$form.Body.value=ticketData.Body
 
 		//// if the above doesn't work, use CKEditor 4 API: https://ckeditor.com/docs/ckeditor4/latest/api/index.html
 		// const ckeditorInstance=window.wrappedJSObject.CKEDITOR.instances.RichText
-		// ckeditorInstance.setData(message.ticketData.Body)
+		// ckeditorInstance.setData(ticketData.Body)
 
 		//// if don't want privileged access through wrappedJSObject, modify the iframe
 		// setTimeout(()=>{
 		// 	const $iframe=document.querySelector('#RichTextField iframe')
-		// 	$iframe.contentDocument.body.innerHTML=message.ticketData.Body
+		// 	$iframe.contentDocument.body.innerHTML=ticketData.Body
 		// },2000)
 
 		//// can also set up MutationObserver instead of dumb timeout above
 	}
-	return Promise.resolve()
+}
+
+function informOnLoginScreen($loginBox) {
+	const id='osmDwgHelperLoginInformBox'
+	if (document.getElementById(id)) return
+	const $informBox=document.createElement('div')
+	$informBox.id=id
+	$informBox.classList.add('ErrorBox')
+	$informBox.innerHTML="<span>Will open a new phone ticket form after a successful login.</span>"
+	$loginBox.prepend($informBox)
 }
 
 function feedValues($input,values) {
