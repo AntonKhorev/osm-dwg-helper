a90f1c467acdef88caa50f272ca19a1a30354874
Anton Khorev <tony29@yandex.ru>
2021-11-29 19:27:45 +0300

    0-centered coords

--- src/icon.js
+++ src/icon.js
@@ -5,19 +5,19 @@ export default function(type) {
 }
 
 function svg(content) {
-	return `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' stroke-width='2'>${content}</svg>`
+	return `<svg xmlns='http://www.w3.org/2000/svg' viewBox='-16 -16 32 32' stroke-width='2'>${content}</svg>`
 }
 
 function tabs() {
 	return (
-		`<polygon points='1,31 1,5 5,5 5,1 23,1 23,5 27,5' fill='rgb(90%,90%,90%)' stroke='rgb(20%,20%,40%)' />`+
-		`<polygon points='31,1 31,27 27,27 27,31 9,31 9,27 1,27' fill='rgb(40%,40%,40%)' stroke='rgb(10%,10%,20%)' />`
+		`<polygon points='-15,15 -15,-11 -11,-11 -11,-15 7,-15 7,-11 11,-11' fill='rgb(90%,90%,90%)' stroke='rgb(20%,20%,40%)' />`+
+		`<polygon points='15,-15 15,11 11,11 11,15 -7,15 -7,11 -11,11' fill='rgb(40%,40%,40%)' stroke='rgb(10%,10%,20%)' />`
 	)
 }
 
 function envelope() {
 	return (
-		`<rect x='5' y='9' width='22' height='14' fill='white' stroke='red' />`+
-		`<polyline points='5,9 16,18 27,9' fill='none' stroke='red' stroke-linecap='round' stroke-linejoin='round' />`
+		`<rect x='-11' y='-7' width='22' height='14' fill='white' stroke='red' />`+
+		`<polyline points='-11,-7 0,2 11,-7' fill='none' stroke='red' stroke-linecap='round' stroke-linejoin='round' />`
 	)
 }
