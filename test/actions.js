bbb4afd5a1a14df713c5ece5532c8a5dc62de444
Anton Khorev <tony29@yandex.ru>
2021-12-08 23:03:11 +0300

    test CommentIssueWithTicketUrl action

--- test/actions.js
+++ test/actions.js
@@ -2,40 +2,75 @@ import {strict as assert} from 'assert'
 
 import * as Actions from '../src/actions.js'
 
+const testSequence=async(input,sequence)=>{
+	for (const item of sequence) {
+		assert.notEqual(input,undefined)
+		input=await item(input)
+	}
+	assert.equal(input,undefined)
+}
+
 describe("Actions.CreateIssueTicket",()=>{
 	it("works with Create Ticket after New Ticket settings",async()=>{
 		const settings={
 			otrs:'OTRS/',
 			osm:'OSM/',
-			ticket_subject:`Issue #\${issue.id}`
+			ticket_subject:`Issue #\${issue.id}`,
+			issue_comment_ticket:`OTRS ticket created: \${ticket.url}`,
 		}
 		const openerTabId=12
 		const issueData={
 			id:2021,
 			url:'OSM/note/2021'
 		}
-		// CreateIssueTicket
-		const action1=new Actions.CreateIssueTicket(openerTabId,issueData)
-		const url1='OTRS/otrs/index.pl?Action=AgentTicketPhone'
-		assert.equal(action1.getActionUrl(settings),url1)
 		const newTabId=23
-		const tab1={id:newTabId,url:url1}
-		const tabState1={}
-		assert.equal(action1.needToRejectUrl(settings,url1),false)
-		let listenerTabId1,listenerScript1,listenerMessage1
-		const listener1=async(tabId,script,message)=>{
-			listenerTabId1=tabId
-			listenerScript1=script
-			listenerMessage1=message
-		}
-		const [tabId2,action2]=await action1.act(settings,tab1,tabState1,listener1)
-		assert.equal(listenerTabId1,newTabId)
-		assert.equal(listenerScript1,'create-ticket')
-		assert.equal(listenerMessage1?.action,'addIssueDataToTicket')
-		assert.equal(listenerMessage1?.ticketData?.Subject,"Issue #2021")
-		assert.equal(tabId2,newTabId)
-		// CommentIssueWithTicketUrl
-		// TODO
+		await testSequence(new Actions.CreateIssueTicket(openerTabId,issueData),[
+		async(action)=>{ // CreateIssueTicket
+			const url='OTRS/otrs/index.pl?Action=AgentTicketPhone'
+			assert.equal(action.getActionUrl(settings),url)
+			const tab={id:newTabId,url}
+			const tabState={}
+			assert.equal(action.needToRejectUrl(settings,url),false)
+			let listenerTabId,listenerScript,listenerMessage
+			const listener=async(tabId,script,message)=>{
+				listenerTabId=tabId
+				listenerScript=script
+				listenerMessage=message
+			}
+			const [tabId2,action2]=await action.act(settings,tab,tabState,listener)
+			assert.equal(listenerTabId,newTabId)
+			assert.equal(listenerScript,'create-ticket')
+			assert.equal(listenerMessage?.action,'addIssueDataToTicket')
+			assert.equal(listenerMessage?.ticketData?.Subject,"Issue #2021")
+			assert.equal(tabId2,newTabId)
+			return action2
+		},async(action)=>{ // CommentIssueWithTicketUrl
+			const ticketId=7
+			const url=`OTRS/otrs/index.pl?Action=AgentTicketPhone;Subaction=Created;TicketID=${ticketId}`
+			assert.equal(action.getActionUrl(settings),undefined)
+			const tab={id:newTabId,url}
+			const tabState={}
+			assert.equal(action.needToRejectUrl(settings,url),false)
+			let listenerTabId,listenerScript,listenerMessage
+			const listener=async(tabId,script,message)=>{
+				listenerTabId=tabId
+				listenerScript=script
+				listenerMessage=message
+			}
+			const [tabId2,action2]=await action.act(settings,tab,tabState,listener)
+			assert.equal(listenerTabId,openerTabId)
+			assert.equal(listenerScript,'issue')
+			assert.deepEqual(listenerMessage,{
+				action:'addComment',
+				comment:`OTRS ticket created: OTRS/otrs/index.pl?Action=AgentTicketZoom;TicketID=${ticketId}`,
+			})
+			assert.equal(tabId2,newTabId)
+			return action2
+		},async(action)=>{ // GoToUrl
+			// const url=`OTRS/otrs/index.pl?Action=AgentTicketZoom;TicketID=${ticketId}`
+			// assert.equal(action.getActionUrl(settings),url)
+			// assert.equal(action.needToRejectUrl(settings,url),false)
+		}])
 	})
 	it("works with Ticket Zoom after New Ticket settings",async()=>{
 		// TODO
