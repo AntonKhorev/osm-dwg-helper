2b7291e14b7ddc363299ef9225c62dd30d7950fe
Anton Khorev <tony29@yandex.ru>
2021-12-30 17:38:17 +0300

    test osm issues link

--- test/panel-actions-new.js
+++ test/panel-actions-new.js
@@ -10,8 +10,16 @@ const createDocumentAndMenuList=()=>{
 	return [document,$menu]
 }
 
+const findItem=($menu,text)=>{
+	for (const $li of $menu.children) {
+		const $item=$li.firstElementChild
+		if (!$item) continue
+		if ($item.innerText.includes(text)) return $item
+	}
+}
+
 describe("panel-actions-new",()=>{
-	it("writes nothing without settings/permissions",async()=>{
+	it("writes nothing without settings/permissions",()=>{
 		const [document,$menu]=createDocumentAndMenuList()
 		const writeNewActionsMenu=makeNewActionsMenuWriter(
 			document,
@@ -27,5 +35,24 @@ describe("panel-actions-new",()=>{
 		const otherTabState={}
 		writeNewActionsMenu($menu,settings,permissions,tabId,tabState,otherTabId,otherTabState)
 		assert.equal($menu.childElementCount,0)
+	})
+	it("writes open issues link if has osm settings",()=>{
+		const [document,$menu]=createDocumentAndMenuList()
+		const writeNewActionsMenu=makeNewActionsMenuWriter(
+			document,
+			()=>{},
+			(createProperties)=>{},
+			(message)=>{}
+		)
+		const settings={osm:'https://myosm.example.com/'}
+		const permissions={}
+		const tabId=1
+		const tabState={}
+		const otherTabId=2
+		const otherTabState={}
+		writeNewActionsMenu($menu,settings,permissions,tabId,tabState,otherTabId,otherTabState)
+		const $item=findItem($menu,'open OSM issues')
+		assert($item)
+		assert.equal($item.href,`https://myosm.example.com/issues?status=open`)
 	})
 })
