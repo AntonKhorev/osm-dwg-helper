f4169f1ba8b4e297077cc4d3649f1c2d7c5895d2
Anton Khorev <tony29@yandex.ru>
2022-01-31 22:35:22 +0300

    fix: pass arguments to ticket article correctly

--- src/content/ticket-article.js
+++ src/content/ticket-article.js
@@ -3,16 +3,17 @@ import otrsFallback from './otrs.js'
 export function addArticleSubjectAndBody(document,subject,body) {
 	const $form=document.getElementById('Compose')
 	if ($form) {
-		populateArticleForm($form,message.subject,message.body)
+		populateArticleForm(document,$form,subject,body)
 		return Promise.resolve()
 	}
 	otrsFallback(document,`Will add an article to a ticket after a successful login.`)
 }
 
-function populateArticleForm($form,subject,body) {
-	$form.Subject.value=subject
-	$form.Subject.dispatchEvent(new Event('change'))
-	$form.Body.value=body
+function populateArticleForm(document,$form,subject,body) {
+	const Event=document.defaultView.Event
+	$form.elements.Subject.value=subject
+	$form.elements.Subject.dispatchEvent(new Event('change'))
+	$form.elements.Body.value=body
 	// ckeditor should load after this field is updated
 	// TODO but create ticket form required a fallback for early load - maybe need it here too?
 }
---
+++ test/content-ticket-article.js
@@ -0,0 +1,40 @@
+import {strict as assert} from 'assert'
+import {JSDOM} from 'jsdom'
+
+import * as contentScript from '../src/content/ticket-article.js'
+
+describe("ticket article content script",()=>{
+	it("throws on unknown webpage",()=>{
+		const {document}=new JSDOM(`<!DOCTYPE html><p>wrong page!`).window
+		assert.throws(()=>{
+			contentScript.addArticleSubjectAndBody(document,"Hello","<p>Hello!</p>")
+		})
+	})
+	it("inserts login message and throws on login page",()=>{
+		const {document}=new JSDOM(`<!DOCTYPE html><div id=LoginBox>login form here</div>`).window
+		assert.throws(()=>{
+			contentScript.addArticleSubjectAndBody(document,"Hello","<p>Hello!</p>")
+		})
+		const $informBox=document.querySelector('#LoginBox .ErrorBox')
+		assert($informBox)
+		assert($informBox.innerHTML.includes('ticket'))
+	})
+	it("populates the form",()=>{
+		const {document}=new JSDOM(
+			`<!DOCTYPE html>`+
+			`<form action="/otrs/index.pl" method="post" enctype="multipart/form-data" name="compose" id="Compose" class="Validate PreventMultipleSubmits">`+
+			`<input type="text" id="Subject" name="Subject" value="" class="W75pc Validate  Validate_Required"/>`+
+			`<textarea id="RichText" class="RichText Validate  Validate_Required" name="Body" rows="15" cols="78"></textarea>`+
+			`</form>`
+		).window
+		contentScript.addArticleSubjectAndBody(document,"Hello","<p>Hello!</p>")
+		assert.equal(
+			document.getElementById('Subject').value,
+			"Hello"
+		)
+		assert.equal(
+			document.getElementById('RichText').value,
+			"<p>Hello!</p>"
+		)
+	})
+})
