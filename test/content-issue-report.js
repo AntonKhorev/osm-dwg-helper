bd853e02505fecb7c06ae5796bbb53c0c0f94a85
Anton Khorev <tony29@yandex.ru>
2022-06-09 18:10:35 +0300

    shorter issue report module tests

--- test/content-issue-report.js
+++ test/content-issue-report.js
@@ -5,25 +5,12 @@ import {JSDOM} from 'jsdom'
 import processReport from '../src/content/issue-report.js'
 
 const issuePageTemplate=String(await fs.readFile('test/issue.html'))
-const expectedLead=[
-	['plain', 'Reported as '],
-	['category', 'spam'],
-	['plain', ' by '],
-	['user', 'testuser'],
-	['plain', ' on 18 May 2021 at 11:54']
-]
 
 describe("issue report module",()=>{
 	it("processes old plaintext reports",()=>{
 		const [document,$report]=prepareDocumentAndReport(`<p>old style plaintext</p>`)
 		const result=processReport(document,$report,()=>{})
-		assert.deepEqual(result,{
-			wasRead: false,
-			lead: expectedLead,
-			text: ['old style plaintext'],
-			category: 'spam',
-			by: 'testuser'
-		})
+		assertReportText(result,'old style plaintext')
 	})
 	it("processes broken richtext single-paragraph reports",()=>{
 		const [document,$report]=prepareDocumentAndReport(
@@ -32,13 +19,7 @@ describe("issue report module",()=>{
 			`<p></p>`
 		)
 		const result=processReport(document,$report,()=>{})
-		assert.deepEqual(result,{
-			wasRead: false,
-			lead: expectedLead,
-			text: ['broken richtext'],
-			category: 'spam',
-			by: 'testuser'
-		})
+		assertReportText(result,'broken richtext')
 	})
 	it("processes broken richtext multi-paragraph reports",()=>{
 		const [document,$report]=prepareDocumentAndReport(
@@ -49,14 +30,9 @@ describe("issue report module",()=>{
 			`<p></p>`
 		)
 		const result=processReport(document,$report,()=>{})
-		assert.deepEqual(result,{
-			wasRead: false,
-			lead: expectedLead,
-			text: ['one','two','three'],
-			category: 'spam',
-			by: 'testuser'
-		})
+		assertReportText(result,'one','two','three')
 	})
+	
 })
 
 function prepareDocumentAndReport(report) {
@@ -64,4 +40,21 @@ function prepareDocumentAndReport(report) {
 	const {document}=new JSDOM(issuePage).window
 	const $report=document.querySelector('#content .row .row .col')
 	return [document,$report]
+}
+
+function assertReportText(result,...text) {
+	const lead=[
+		['plain', 'Reported as '],
+		['category', 'spam'],
+		['plain', ' by '],
+		['user', 'testuser'],
+		['plain', ' on 18 May 2021 at 11:54']
+	]
+	assert.deepEqual(result,{
+		wasRead: false,
+		lead,
+		text,
+		category: 'spam',
+		by: 'testuser'
+	})
 }
