cde2b134a0bdaba8a86110c922aa304348725b6d
Anton Khorev <tony29@yandex.ru>
2024-05-23 17:03:43 +0300

    join issue report and comment text templates

--- test/content-issue.js
+++ test/content-issue.js
@@ -12,12 +12,10 @@ const unmarkedLead=`Reported as spam by <a href="/user/testuser">testuser</a> on
 
 describe("issue content script",()=>{
 	it("adds comment",()=>{
-		const {document}=new JSDOM(
-			`<!DOCTYPE html>`+
-			`<form role="form" class="new_issue_comment" id="new_issue_comment" action="/issues/18381/comments" accept-charset="UTF-8" method="post">`+
-			`<textarea cols="80" rows="20" class="form-control form-control" data-preview-url="https://www.openstreetmap.org/preview/markdown" name="issue_comment[body]" id="issue_comment_body">Already written.</textarea>`,
-			`</form>`
-		).window
+		const issuePage=issuePageTemplate.replace(
+			'{{report-blocks}}',''
+		)
+		const {document}=new JSDOM(issuePage).window
 		contentScript.addComment(document,"Something else.")
 		assert.equal(
 			document.getElementById('issue_comment_body').value,
