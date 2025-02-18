9ff933e92e9349833cf451b87b8a881504dfc95e
Anton Khorev <tony29@yandex.ru>
2025-02-18 05:46:39 +0300

    remove references to "ticket" from go to input

--- src/go-to-object.js
+++ src/go-to-object.js
@@ -3,18 +3,18 @@ import goToObjectParser from './go-to-object-parser.js'
 export default function ($form,settings,tabId,closeWindow,createTab) {
 	if (settings.otrs || settings.osm) {
 		$form.hidden=false
-		$form.ticket.oninput=()=>{
-			const value=$form.ticket.value
+		$form.query.oninput=()=>{
+			const value=$form.query.value
 			const parsedValue=goToObjectParser(value)
 			if (value=="" || parsedValue.site && parsedValue.path) {
-				$form.ticket.setCustomValidity("")
+				$form.query.setCustomValidity("")
 			} else {
-				$form.ticket.setCustomValidity("ticket id or number required")
+				$form.query.setCustomValidity("ticket id or number required")
 			}
 		}
 		$form.onsubmit=(ev)=>{
 			ev.preventDefault()
-			const value=$form.ticket.value
+			const value=$form.query.value
 			const parsedValue=goToObjectParser(value)
 			let url
 			if (parsedValue.site=="otrs" && settings.otrs) {
@@ -30,7 +30,7 @@ export default function ($form,settings,tabId,closeWindow,createTab) {
 		}
 	} else {
 		$form.hidden=true
-		$form.ticket.oninput=undefined
+		$form.query.oninput=undefined
 		$form.onsubmit=(ev)=>{
 			ev.preventDefault()
 		}
